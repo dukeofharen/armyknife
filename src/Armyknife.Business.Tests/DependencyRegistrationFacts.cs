75ca0b48f715819ca17e8679d9b2a0b2048ceb60
Duco Winterwerp <duco@winterwerp.nl>
2017-07-30 23:27:22 +0200

    Added DependencyRegistration unit tests

---
+++ src/Armyknife.Business.Tests/DependencyRegistrationFacts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Armyknife.Business.Tests
+{
+    [TestClass]
+    public class DependencyRegistrationFacts
+    {
+        private IServiceProvider _serviceProvider;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var serviceCollection = new ServiceCollection();
+            DependencyRegistration.RegisterDependencies(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        [TestMethod]
+        public void DependencyRegistration_ResolveExecutor_HappyFlow()
+        {
+            // act
+            var executor = _serviceProvider.GetService<IExecutor>();
+
+            // assert
+            Assert.IsNotNull(executor);
+        }
+    }
+}
