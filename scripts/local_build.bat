3e876bbe92e5e6912db512e695c3981bab55e18b
Duco Winterwerp <duco@winterwerp.nl>
2017-08-27 20:28:28 +0200

    Fixed build script (hopefully)

---
+++ scripts/local_build.bat
@@ -0,0 +1,4 @@
+:: %APPVEYOR_BUILD_NUMBER% = 1337
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& %~dp0patch_versions.ps1"
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& %~dp0build.ps1"
+pause
