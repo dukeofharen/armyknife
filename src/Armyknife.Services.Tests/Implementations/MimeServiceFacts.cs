c6d17a3b02739adbfe8e6e1ac8d0329a4ac0574e
Duco Winterwerp <duco@winterwerp.nl>
2017-08-27 22:35:02 +0200

    Added MimeService

---
+++ src/Armyknife.Services.Tests/Implementations/MimeServiceFacts.cs
@@ -0,0 +1,115 @@
+﻿using Armyknife.Services.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Armyknife.Services.Tests.Implementations
+{
+    [TestClass]
+    public class MimeServiceFacts
+    {
+        private MimeService _service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _service = new MimeService();
+        }
+
+        [TestMethod]
+        public void MimeService_GetMimeType_HappyFlow()
+        {
+            // arrange
+            string extension = "txt";
+            string expectedMimeType = "text/plain";
+
+            // act
+            string result = _service.GetMimeType(extension);
+
+            // assert
+            Assert.AreEqual(expectedMimeType, result);
+        }
+
+        [TestMethod]
+        public void MimeService_GetMimeType_HappyFlow_InputIsFilename()
+        {
+            // arrange
+            string extension = "file.txt";
+            string expectedMimeType = "text/plain";
+
+            // act
+            string result = _service.GetMimeType(extension);
+
+            // assert
+            Assert.AreEqual(expectedMimeType, result);
+        }
+
+        [TestMethod]
+        public void MimeService_GetMimeType_HappyFlow_MimeTypeNotFound_ShouldReturnFallbackMimeType()
+        {
+            // arrange
+            string extension = "blablabla";
+            string expectedMimeType = "application/octet-stream";
+
+            // act
+            string result = _service.GetMimeType(extension);
+
+            // assert
+            Assert.AreEqual(expectedMimeType, result);
+        }
+
+        [TestMethod]
+        public void MimeService_GetMimeType_HappyFlow_InputIsEmpty_ShouldReturnFallbackMimeType()
+        {
+            // arrange
+            string extension = string.Empty;
+            string expectedMimeType = "application/octet-stream";
+
+            // act
+            string result = _service.GetMimeType(extension);
+
+            // assert
+            Assert.AreEqual(expectedMimeType, result);
+        }
+
+        [TestMethod]
+        public void MimeService_GetExtension_HappyFlow()
+        {
+            // arrange
+            string mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            string expectedExtension = "xlsx";
+
+            // act
+            var result = _service.GetExtensions(mimeType).ToArray();
+
+            // assert
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(expectedExtension, result.First());
+        }
+
+        [TestMethod]
+        public void MimeService_GetExtension_MimeTypeNotFound_ShouldReturnEmptyArray()
+        {
+            // arrange
+            string mimeType = "text/unknown-mime";
+
+            // act
+            var result = _service.GetExtensions(mimeType).ToArray();
+
+            // assert
+            Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void MimeService_GetExtension_InputIsEmpty_ShouldReturnEmptyArray()
+        {
+            // arrange
+            string mimeType = string.Empty;
+
+            // act
+            var result = _service.GetExtensions(mimeType).ToArray();
+
+            // assert
+            Assert.AreEqual(0, result.Length);
+        }
+    }
+}
---
+++ src/Armyknife.Services/IMimeService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Armyknife.Services
+{
+    public interface IMimeService
+    {
+        string GetMimeType(string input);
+
+        IEnumerable<string> GetExtensions(string mimeType);
+    }
+}
