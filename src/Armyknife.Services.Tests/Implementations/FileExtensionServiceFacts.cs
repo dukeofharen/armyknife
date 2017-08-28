39a8eb93ea3a1531fd2da7be8cecb64329119cd6
Duco Winterwerp <duco@winterwerp.nl>
2017-08-28 21:30:36 +0200

    Added FileExtensionService

---
+++ src/Armyknife.Services.Tests/Implementations/FileExtensionServiceFacts.cs
@@ -0,0 +1,66 @@
+﻿using Armyknife.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Armyknife.Services.Tests.Implementations
+{
+    [TestClass]
+    public class FileExtensionServiceFacts
+    {
+        private FileExtensionService _service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var serviceCollection = new ServiceCollection();
+            DependencyRegistration.RegisterDependencies(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            _service = (FileExtensionService)serviceProvider.GetService<IFileExtensionService>();
+        }
+
+        [TestMethod]
+        public void FileExtensionService_GetFileExtensionInfo_HappyFlow()
+        {
+            // arrange
+            string extension = "TXT";
+
+            // act
+            var result = _service.GetFileExtensionInfo(extension);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("TXT", result.Extension);
+            Assert.AreEqual("Common name for ASCII text file", result.Description);
+            Assert.AreEqual("Microsoft Notepad", result.UsedBy);
+        }
+
+        [TestMethod]
+        public void FileExtensionService_GetFileExtensionInfo_HappyFlow_ExtensionLowerCase()
+        {
+            // arrange
+            string extension = "txt";
+
+            // act
+            var result = _service.GetFileExtensionInfo(extension);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("TXT", result.Extension);
+            Assert.AreEqual("Common name for ASCII text file", result.Description);
+            Assert.AreEqual("Microsoft Notepad", result.UsedBy);
+        }
+
+        [TestMethod]
+        public void FileExtensionService_GetFileExtensionInfo_NotFound_ShouldReturnNull()
+        {
+            // arrange
+            string extension = "ragsdfaeswtygaerg";
+
+            // act
+            var result = _service.GetFileExtensionInfo(extension);
+
+            // assert
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ src/Armyknife.Services/Models/FileExtensionInfoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Armyknife.Services.Models
+{
+    public class FileExtensionInfoModel
+    {
+        public string Extension { get; set; }
+
+        public string Description { get; set; }
+
+        public string UsedBy { get; set; }
+    }
+}
