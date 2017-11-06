ea68bcf048d907b3a15971781500cae3aa0d93bc
Duco <duco@winterwerp.it>
2017-11-06 23:38:52 +0100

    Added Base64EncodeTool integration test

---
+++ src/Armyknife.IntegrationTests/Tools/Base64EncodeToolIntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Armyknife.IntegrationTests.Tools
+{
+   [TestClass]
+   public class Base64EncodeToolIntegrationTests : IntegrationTestBase
+   {
+      [TestMethod]
+      public async Task Base64EncodeTool_IntegrationTest()
+      {
+         // arrange
+         var args = GetArgs($"base64encode this is the input");
+         string expectedOutput = "dGhpcyBpcyB0aGUgaW5wdXQ=";
+
+         // act
+         await _executor.ExecuteAsync(args);
+
+         // assert
+         Assert.AreEqual(expectedOutput, _output);
+      }
+   }
+}
