b0089b825f4982505942abcec517ffbcd7636813
Duco <duco@winterwerp.it>
2017-11-19 19:13:49 +0100

    Added integration test for HelpTool

---
+++ src/Armyknife.Integration.Tests/Business/Tools/HelpToolIntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace Armyknife.Integration.Tests.Business.Tools
+{
+   [TestClass]
+   public class HelpToolIntegrationTest : IntegrationTestBase
+   {
+      [TestMethod]
+      public async Task HelpTool_IntegrationTest_GenericHelp()
+      {
+         // arrange
+         var args = GetArgs($"help");
+
+         // act
+         await _executor.ExecuteAsync(args);
+
+         // assert
+         Assert.IsTrue(_output.Split(Environment.NewLine).Length > 1);
+      }
+
+      [TestMethod]
+      public async Task HelpTool_IntegrationTest_SpecificHelp()
+      {
+         // arrange
+         var args = GetArgs($"help base64encode");
+
+         // act
+         await _executor.ExecuteAsync(args);
+
+         // assert
+         Assert.IsTrue(_output.Split(Environment.NewLine).Length > 1);
+      }
+   }
+}
