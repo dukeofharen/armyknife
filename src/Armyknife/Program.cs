a279fab65e1efa55e61a33b4c0e441f3fc273924
Duco <duco@winterwerp.it>
2017-11-25 23:37:55 +0100

    Updated Program.cs to use the latest C# version so we can use async Main method

--- src/Armyknife/Program.cs
+++ src/Armyknife/Program.cs
@@ -2,19 +2,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using Armyknife.Business.Interfaces;
 using System;
+using System.Threading.Tasks;
 
 namespace Armyknife
 {
    class Program
    {
-      static void Main(string[] args)
+      static async Task Main(string[] args)
       {
          var serviceCollection = new ServiceCollection();
          DependencyRegistration.RegisterDependencies(serviceCollection);
          var provider = serviceCollection.BuildServiceProvider();
 
          var executor = provider.GetService<IExecutor>();
-         int exitCode = executor.ExecuteAsync(args).Result;
+         int exitCode = await executor.ExecuteAsync(args);
          Environment.Exit(exitCode);
       }
    }
