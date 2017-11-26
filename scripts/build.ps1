c68ffefeb34e3755403e48ae3ca455b5772e306d
Duco <duco@winterwerp.it>
2017-11-26 16:44:33 +0100

    Updated solution so OpenCover coverage is run on build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,10 +3,15 @@ $ErrorActionPreference = 'Stop'
 $rootFolder = Join-Path -Path $PSScriptRoot ".."
 $srcFolder = Join-Path -Path $rootFolder "src"
 $mainProjectFile = Join-Path $srcFolder "Armyknife\Armyknife.csproj"
+$solutionFile = Join-Path -Path $srcFolder "Armyknife.sln"
 $nsiPath = Join-Path $PSScriptRoot "armyknife.nsi"
 $winBinDir = Join-Path $srcFolder "Armyknife\bin\release\netcoreapp2.0\win10-x64\publish"
 $installScriptsPath = Join-Path -Path $PSScriptRoot "installscripts"
 $unitTestPath = Join-Path -Path $srcFolder "Armyknife.Tests\Armyknife.Tests.csproj"
+$openCoverPath = Join-Path -Path $env:USERPROFILE ".nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe"
+$reportGeneratorPath = Join-Path -Path $env:USERPROFILE  ".nuget\packages\reportgenerator\3.0.2\tools\ReportGenerator.exe"
+$openCoverReportPath = Join-Path -Path $rootFolder "opencover"
+$openCoverReportFilePath = Join-Path -Path $openCoverReportPath "OpenCover.xml"
 
 $nsisPath = "C:\Program Files (x86)\NSIS\Bin"
 
@@ -20,11 +25,29 @@ $env:PATH = "$env:PATH;$nsisPath"
 Write-Host "Cleaning the solution"
 Get-ChildItem $srcFolder -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 
-# Run unit tests
+# Perform a debug build
+& dotnet build $solutionFile /p:DebugType=Full
+
+# Run unit tests and calculate code coverage
+if(!(Test-Path $openCoverReportPath))
+{
+    Write-Host "Creating directory $openCoverReportPath" 
+    New-item -ItemType Directory -Path $openCoverReportPath
+}
+else
+{
+    Write-Host "Deleting directory $openCoverReportPath" 
+    Remove-Item -Path $openCoverReportPath -Recurse -Force
+
+    Write-Host "Creating directory $openCoverReportPath" 
+    New-item -ItemType Directory -Path $openCoverReportPath
+}
+
 Write-Host "Running unit test project $unitTestPath"
-& dotnet restore $unitTestPath
+& dotnet build $solutionFile /p:DebugType=Full
+& $openCoverPath -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test $unitTestPath --configuration Debug --no-build" -filter:"+[Armyknife*]* -[*.Tests*]*" -oldStyle -register:user -output:"$openCoverReportFilePath" -returntargetcode:1
 Assert-Cmd-Ok
-& dotnet test $unitTestPath
+& $reportGeneratorPath -reports:$openCoverReportFilePath -targetdir:$openCoverReportPath -verbosity:Error
 Assert-Cmd-Ok
 
 # Release package build
@@ -58,11 +81,6 @@ Write-Host "Building installer $nsiPath"
 Assert-Cmd-Ok
 
 # Patch documentation
-$docGenProjectFile = Join-Path -Path $srcFolder "Armyknife.DocGenerator\Armyknife.DocGenerator.csproj"
-Write-Host "Building project $docGenProjectFile"
-& dotnet build $docGenProjectFile
-Assert-Cmd-Ok
-
 $docGenDllFile = Join-Path -Path $srcFolder "Armyknife.DocGenerator\bin\Debug\netcoreapp2.0\Armyknife.DocGenerator.dll"
 Write-Host "Now running $docGenDllFile"
 & dotnet $docGenDllFile
