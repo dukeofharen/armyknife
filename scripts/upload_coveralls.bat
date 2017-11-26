7940884eba27d3de1eb34f50d32f0d0e2c04a2bd
Duco <duco@winterwerp.it>
2017-11-26 17:21:43 +0100

    Added coveralls runner to unit test project

--- scripts/upload_coveralls.bat
+++ scripts/upload_coveralls.bat
@@ -1,1 +1,3 @@
-%USERPROFILE%\.nuget\packages\coveralls.io\1.4.2\tools\coveralls.net.exe --opencover %~dp0..\opencover\OpenCover.xml -r %COVERALLS_REPO_TOKEN%
+SET COVERALLSIO=%USERPROFILE%\.nuget\packages\coveralls.io\1.4.2\tools\coveralls.net.exe
+ECHO %COVERALLSIO%
+%COVERALLSIO% --opencover %~dp0..\opencover\OpenCover.xml -r %COVERALLS_REPO_TOKEN%
